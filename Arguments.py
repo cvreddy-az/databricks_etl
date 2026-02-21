a3f2aa7dd95187087f70bddae976f01b8ade84d5
Venkateswara Reddy <cvreddy_az@outlook.com>
2026-02-21 19:18:09 +0530

    Python arbitary arguments

---
+++ Arguments.py
@@ -0,0 +1,6 @@
+def accept_anything(*args, **kwargs):
+    print("Positional args:", args)
+    print("Keyword args:", kwargs)
+
+# Example usage
+accept_anything(1, "hello", True, name="Lenovo", age=25)
